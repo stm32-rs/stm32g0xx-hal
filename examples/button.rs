0dac5f9bd8103fb1dd2210da6131e2ab487b19b8
Vitaly Domnikov <dotcypress@gmail.com>
2019-01-13 20:36:02 -0800

    add button example

--- examples/button.rs
+++ examples/button.rs
@@ -9,34 +9,26 @@ extern crate panic_semihosting;
 extern crate stm32g0xx_hal as hal;
 
 use hal::prelude::*;
-use hal::rcc::{RccConfig, SysClockSrc};
 use hal::stm32;
-use rt::{entry, exception, ExceptionFrame};
+use rt::entry;
 
 #[entry]
 fn main() -> ! {
-    hal::debug::init();
     let dp = stm32::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
-    let mut rcc = dp.RCC.freeze(RccConfig::new(SysClockSrc::LSI));
+
+    let mut rcc = dp.RCC.constrain();
+    let mut delay = cp.SYST.delay(&rcc.clocks);
+
+    let gpioc = dp.GPIOC.split(&mut rcc);
+    let button = gpioc.pc13.into_pull_up_input();
 
     let gpioa = dp.GPIOA.split(&mut rcc);
     let mut led = gpioa.pa5.into_push_pull_output();
 
-    let mut delay = cp.SYST.delay(&rcc.clocks);
-
     loop {
+        let wait = if button.is_high() { 300.ms() } else { 100.ms() };
+        delay.delay(wait);
         led.toggle();
-        delay.delay(500.ms());
     }
-}
-
-#[exception]
-fn HardFault(ef: &ExceptionFrame) -> ! {
-    panic!("Hard fault {:#?}", ef);
-}
-
-#[exception]
-fn DefaultHandler(irqn: i16) {
-    panic!("Unhandled exception (IRQn = {})", irqn);
 }
