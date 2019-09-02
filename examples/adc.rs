3e1340e94ae14c5d0d86fced82eea5348744e63f
Vitaly Domnikov <dotcypress@gmail.com>
2019-09-01 22:57:15 -0700

    update ADC example

--- examples/adc.rs
+++ examples/adc.rs
@@ -9,7 +9,7 @@ extern crate panic_halt;
 extern crate stm32g0xx_hal as hal;
 
 use cortex_m_semihosting::hprintln;
-use hal::adc::{SampleTime, VTemp};
+use hal::adc::{SampleTime, Precision, VTemp};
 use hal::prelude::*;
 use hal::stm32;
 use rt::entry;
@@ -18,21 +18,23 @@ use rt::entry;
 fn main() -> ! {
     let dp = stm32::Peripherals::take().expect("cannot take peripherals");
     let mut rcc = dp.RCC.constrain();
+
     let gpioa = dp.GPIOA.split(&mut rcc);
 
     let mut adc = dp.ADC.constrain(&mut rcc);
-    adc.set_sample_time(SampleTime::T_160);
+    adc.set_sample_time(SampleTime::T_80);
+    adc.set_precision(Precision::B_12);
 
     let mut adc_pin = gpioa.pa0.into_analog();
     let mut vtemp = VTemp::new();
     vtemp.enable(&mut adc);
 
     loop {
-        let u: u32 = adc.read(&mut adc_pin).expect("adc read failed");
+        let u_raw: u32 = adc.read(&mut adc_pin).expect("adc read failed");
         let temp: u32 = adc.read(&mut vtemp).expect("temperature read failed");
 
-        let u = 3300 * u / 4096;
+        let u = u_raw.saturating_sub(32) as f32 / 4_096_f32 * 3.3;
         let temp = temp / 42;
-        hprintln!("u: {:?} mV | t: {:?}°C", u, temp).unwrap();
+        hprintln!("u: {:.2} V | t: {:?}°C", u, temp).unwrap();
     }
 }
