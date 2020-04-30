824a2142bc5f45d1e6f2e8f84c909f348d075ce8
Vitaly Domnikov <dotcypress@gmail.com>
2020-04-30 09:24:31 -0700

    low power mode

--- src/rcc/mod.rs
+++ src/rcc/mod.rs
@@ -192,6 +192,18 @@ impl Rcc {
         }
     }
 
+    pub fn enable_low_power_mode(&self) {
+        self.rb.apbenr1.modify(|_, w| w.pwren().set_bit());
+        let pwr = unsafe { &(*PWR::ptr()) };
+        pwr.cr1.modify(|_, w| w.lpr().set_bit());
+    }
+    
+    pub fn disable_low_power_mode(&self) {
+        self.rb.apbenr1.modify(|_, w| w.pwren().set_bit());
+        let pwr = unsafe { &(*PWR::ptr()) };
+        pwr.cr1.modify(|_, w| w.lpr().clear_bit());
+    }
+
     fn config_pll(&self, pll_cfg: PllConfig) -> PLLClocks {
         assert!(pll_cfg.m > 0 && pll_cfg.m <= 8);
         assert!(pll_cfg.r > 1 && pll_cfg.r <= 8);
