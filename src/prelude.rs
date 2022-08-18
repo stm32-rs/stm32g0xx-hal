2cf638d3436cb1bd57da620c4fe857be6a668b6c
David Lattimore <dml@google.com>
2022-08-19 06:12:50 +1000

    Add support for using PLLQ with TIM1/TIM15 (#115)

--- src/prelude.rs
+++ src/prelude.rs
@@ -39,6 +39,7 @@ pub use crate::spi::SpiExt as _;
 pub use crate::time::U32Ext as _;
 pub use crate::timer::opm::OpmExt as _;
 pub use crate::timer::pwm::PwmExt as _;
+pub use crate::timer::pwm::PwmQExt as _;
 pub use crate::timer::qei::QeiExt as _;
 pub use crate::timer::stopwatch::StopwatchExt as _;
 pub use crate::timer::TimerExt as _;
